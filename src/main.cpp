c1484f7681f3973fb9d5cc7f4f50a03e5ab0303c
koloshmet-hse <mvguzov@edu.hse.ru>
2019-10-01 15:55:55 +0300

    Colorized output

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 
 #include <util/tree_value/json_io.h>
 
+#include <util/string/utils.h>
 #include <util/exception/exception.h>
 
 #include <fstream>
@@ -30,6 +31,53 @@ std::ifstream GetConfig() {
 
 using namespace std::literals;
 
+void PrintTabs(size_t count) {
+    for (size_t i = 0; i < count * 2; ++i) {
+        std::cout << ' ';
+    }
+}
+
+std::string WrapDir(std::string_view str, bool finished = false) {
+    std::string res;
+    if (!finished) {
+        res += "\033[1;33m";
+    } else {
+        res += "\033[1;32m";
+    }
+    res += str;
+    res += "\033[0m";
+    return res;
+}
+
+void ProcessResult(TSubprocess& runningScp) {
+    std::vector<std::string> dirStack;
+    for (std::string curSent; std::getline(runningScp.Err(), curSent);) {
+        std::string_view message{curSent};
+        auto pattern = "Sink:"sv;
+        if (message.substr(0, pattern.length()) == pattern) {
+            auto mainPart = message.substr(pattern.length() + 1);
+            std::vector<std::string_view> parts = Split(mainPart, " ");
+
+            if (parts.size() == 1) {
+                if (parts.front() == "E") {
+                    PrintTabs(dirStack.size() - 1);
+                    std::cout << WrapDir(dirStack.back(), true) << std::endl;
+                    dirStack.pop_back();
+                } else {
+                    std::cout << parts.front() << std::endl;
+                }
+            } else if (parts[1] == "0") {
+                PrintTabs(dirStack.size());
+                std::cout << WrapDir(parts.back()) << std::endl;
+                dirStack.emplace_back(parts.back());
+            } else {
+                PrintTabs(dirStack.size());
+                std::cout << parts.back() << std::endl;
+            }
+        }
+    }
+}
+
 int main(int argc, char* argv[]) {
     TTreeValue configJson;
     try {
@@ -56,14 +104,7 @@ int main(int argc, char* argv[]) {
             }
         }();
 
-        std::string curSent;
-        while (std::getline(runningScp.Err(), curSent)) {
-            std::string_view message{curSent};
-            auto pattern = "Sink:"sv;
-            if (message.substr(0, pattern.length()) == pattern) {
-                std::cout << message.substr(pattern.length()) << std::endl;
-            }
-        }
+        ProcessResult(runningScp);
         runningScp.Wait();
     } catch (const std::exception& exception) {
         std::cerr << exception.what() << std::endl;
