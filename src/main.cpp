bf5427abc2f07a2ec848ac0cb6e044aebf9b369c
koloshmet-hse <mvguzov@edu.hse.ru>
2019-10-01 22:33:27 +0300

    Added searching for local config

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 #include <fstream>
 #include <iostream>
 
-std::ifstream GetConfig() {
+std::ifstream GetGlobalConfig() {
     std::filesystem::path configPath(std::getenv("HOME"));
     configPath /= ".scpcopier";
     if (!std::filesystem::exists(configPath)) {
@@ -29,59 +29,26 @@ std::ifstream GetConfig() {
     return std::ifstream{configPath};
 }
 
-using namespace std::literals;
-
-void PrintTabs(size_t count) {
-    for (size_t i = 0; i < count * 2; ++i) {
-        std::cout << ' ';
-    }
-}
-
-std::string WrapDir(std::string_view str, bool finished = false) {
-    std::string res;
-    if (!finished) {
-        res += "\033[1;33m";
-    } else {
-        res += "\033[1;32m";
-    }
-    res += str;
-    res += "\033[0m";
-    return res;
-}
-
-void ProcessResult(TSubprocess& runningScp) {
-    std::vector<std::string> dirStack;
-    for (std::string curSent; std::getline(runningScp.Err(), curSent);) {
-        std::string_view message{curSent};
-        auto pattern = "Sink:"sv;
-        if (message.substr(0, pattern.length()) == pattern) {
-            auto mainPart = message.substr(pattern.length() + 1);
-            std::vector<std::string_view> parts = Split(mainPart, " ");
-
-            if (parts.size() == 1) {
-                if (parts.front() == "E") {
-                    PrintTabs(dirStack.size() - 1);
-                    std::cout << WrapDir(dirStack.back(), true) << std::endl;
-                    dirStack.pop_back();
-                } else {
-                    std::cout << parts.front() << std::endl;
-                }
-            } else if (parts[1] == "0") {
-                PrintTabs(dirStack.size());
-                std::cout << WrapDir(parts.back()) << std::endl;
-                dirStack.emplace_back(parts.back());
-            } else {
-                PrintTabs(dirStack.size());
-                std::cout << parts.back() << std::endl;
+std::ifstream GetLocalConfig() {
+    const std::filesystem::path home = std::getenv("HOME");
+    for (auto curPath = std::filesystem::current_path(); curPath != home; curPath = curPath.parent_path()) {
+        auto local = curPath / ".scpcopier";
+        if (std::filesystem::exists(local) && std::filesystem::is_directory(local)) {
+            local /= "config.json";
+            if (std::filesystem::exists(local) && std::filesystem::is_regular_file(local)) {
+                return std::ifstream{local};
             }
         }
     }
+    return GetGlobalConfig();
 }
+
+using namespace std::literals;
 
 int main(int argc, char* argv[]) {
     TTreeValue configJson;
     try {
-        auto config = GetConfig();
+        auto config = GetLocalConfig();
         config >> TJsonIO{configJson};
     } catch (const std::exception& exception) {
         std::cerr << exception.what() << std::endl;
@@ -92,20 +59,16 @@ int main(int argc, char* argv[]) {
         if (argc < 2) {
             throw TException{"Put one argument 'u' - Upload or 'd' - Download"};
         }
-        auto runningScp = [&scp, argv] {
-            std::string arg{argv[1]};
-            if (arg.empty() || (std::tolower(arg.front()) != 'u' && std::tolower(arg.front()) != 'd')) {
-                throw TException{"Put one argument 'u' - Upload or 'd' - Download"};
-            }
-            if (std::tolower(arg.front()) == 'u') {
-                return scp.Upload();
-            } else {
-                return scp.Download();
-            }
-        }();
+        std::string_view arg{argv[1]};
+        if (arg.empty() || (std::tolower(arg.front()) != 'u' && std::tolower(arg.front()) != 'd')) {
+            throw TException{"Put one argument 'u' - Upload or 'd' - Download"};
+        }
 
-        ProcessResult(runningScp);
-        runningScp.Wait();
+        if (std::tolower(arg.front()) == 'u') {
+            scp.Upload(std::cout);
+        } else {
+            scp.Download(std::cout);
+        }
     } catch (const std::exception& exception) {
         std::cerr << exception.what() << std::endl;
     }
