48fe12b08310714fd0e680582eff090fc1e1bdaf
koloshmet-hse <mvguzov@edu.hse.ru>
2019-10-08 23:33:38 +0300

    Added git support

--- src/scp.cpp
+++ src/scp.cpp
@@ -1,16 +1,49 @@
 #include "scp.h"
 
+#include <vcs.h>
+
 #include <util/string/utils.h>
 #include <util/exception/exception.h>
 
+TVcs::EMode GetMode(std::string_view mode) {
+    switch (std::tolower(mode.front())) {
+        case 'a':
+            return TVcs::EMode::Added;
+        case 'm':
+            return TVcs::EMode::Modified;
+        default:
+            return TVcs::EMode::All;
+    }
+}
+
 TScp::TScp(const TTreeValue& config)
     : Executable(ToString(config["scp_executable"]))
-    , Files{config["files"].AsArray().begin(), config["files"].AsArray().end()}
+    , Files{}
     , Target{ToString(config["target_address"])}
     , Login{ToString(config["login"])}
     , SourceRoot{ToString(config["source_root"])}
     , TargetRoot{ToString(config["target_root"])}
-{}
+{
+    bool preferVcs = config.Contains("vcs") && !config.Contains("files");
+    if (config.Contains("vcs") && config.Contains("files")) {
+        if (config.Contains("prefer_vcs")) {
+            preferVcs = static_cast<bool>(config["prefer_vcs"]);
+        }
+    }
+
+    if (preferVcs) {
+        TVcs vcs{ToString(config["vcs"]["path"]), GetMode(ToString(config["vcs"]["mode"]))};
+        auto files = vcs.Status();
+
+        for (auto&& cur : files) {
+            Files.push_back(cur.string());
+        }
+    } else {
+        for (auto&& cur : config["files"].AsArray()) {
+            Files.push_back(ToString(cur));
+        }
+    }
+}
 
 TSubprocess TScp::Upload() const {
     if (!std::filesystem::exists(SourceRoot)) {
